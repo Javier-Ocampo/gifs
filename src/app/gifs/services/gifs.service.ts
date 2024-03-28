cff0777cfa1cd8821453207f324eaddc17052e49
Javier Ocampo <info@javierocampo.net>
2024-03-28 20:00:41 +0100

    added pages

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchResponse, Gif } from '../interfaces/gifs.interfaces';
+
+@Injectable({ providedIn: 'root'})
+export class GifsService {
+
+  public gifList: Gif[] = [];
+
+  private _tagsHistory: string[] = [];
+  private apiKey: string = '6uuFQn02DQKN9tfQhpl7yvKLXyD3du75';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ) { }
+
+  get tagsHistory(): string[] {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+    }
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((response) => {
+        this.gifList = response.data;
+        console.log({ gifs: this.gifList });
+      });
+  }
+
+}
